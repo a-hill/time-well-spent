8f481fd6371c9f6af2b89e162f4a0a8d8da3889d
riajha <ria.jha15@imperial.ac.uk>
2017-10-26 17:29:09 +0100

    Added some more comments to the function for other group members for future ussage

--- lib/videoInterface.py
+++ lib/videoInterface.py
@@ -3,17 +3,17 @@ import cv2
 import time
 
 cameraPort = 0
-
+#TODO: Refactor so that cap is passed in as a parameter
 def getFrameFromVideo(videoInput):
 	cap = cv2.VideoCapture(videoInput)
-	time.sleep(3)
+	if not isinstance(videoInput, str):
+		#Allow time for webcam to wake up
+		time.sleep(2)
 	ret, frame = cap.read()
 
 	if not ret:
 		print "Failed to get frame from video input "
 		print(videoInput)
-	cv2.imshow('frame', frame)
-	cv2.waitKey(10000)
 	cap.release()
 	return frame
 
