97461cd88bbd58e44bb0684ba460276b981ed99d
Andrew Hill <andrew.hill15@imperial.ac.uk>
2017-10-27 17:23:26 +0100

    Implements creating representation of a face from a frame.

---
+++ FaceRecognition.py
@@ -0,0 +1,34 @@
+import cv2
+import openface
+
+class FaceRecognition():
+
+    def __init__(self, facePredictorPath, alignedImageDimensions, networkModelPath):
+        self.facePredictorPath = facePredictorPath
+        self.alignedImgDimensions = alignedImageDimensions
+        self.aligner = openface.AlignDlib(facePredictorPath)
+        self.net = openface.TorchNeuralNet(networkModelPath, alignedImageDimensions)
+
+    def get_rep(self, image, imgDim):
+
+        alignedFace = self.align_face(image, imgDim)
+        rep = self.net.forward(alignedFace)
+        return rep
+
+    def align_face(self, image, imgDim):
+        # Converts image to format expected by aligner
+        rgbImg = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # Finds biggest face in frame and gets the box around it
+        bb = self.aligner.getLargestFaceBoundingBox(rgbImg)
+
+        if bb is None:  # No face found in frame
+            return None
+
+        # Crops and rotates according to bb
+        alignedFace = self.aligner.align(imgDim, rgbImg, bb, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+
+        if alignedFace is None:  # Alignment failed
+            return None
+
+        return alignedFace
--- TestOneInOneOut.py
+++ TestOneInOneOut.py
@@ -1,14 +1,25 @@
 import unittest
+
+from FaceRecognition import FaceRecognition
 from VideoInterface import VideoInterface
 import numpy as np
 
 
 class TestOneInOneOut(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(self):
+        self.defaultImageDims = 96
+        self.pathToDLibFacePredictor = './../openface/models/dlib/shape_predictor_68_face_landmarks.dat'
+        self.pathToTorchNeuralNet = './../openface/models/openface/nn4.small2.v1.t7'
+        self.faceRecognition = FaceRecognition(self.pathToDLibFacePredictor, self.defaultImageDims, self.pathToTorchNeuralNet)
+        self.testImagePath = './alessio.jpg'
+        self.testVideoPath = "./video.mp4"
+
+
     def test_can_read_image_from_file(self):
-        pathToFrame = './alessio.jpg'
         videoInterface = VideoInterface()
-        testImage = videoInterface.get_image_from_file(pathToFrame)
+        testImage = videoInterface.get_image_from_file(self.testImagePath)
         self.assertIsInstance(testImage, np.ndarray)
 
     def test_can_read_frame_from_webcam(self):
@@ -17,19 +28,15 @@ class TestOneInOneOut(unittest.TestCase):
         self.assertIsInstance(frame, np.ndarray)
 
     def test_can_read_frame_from_video(self):
-        pathToVideo = "./video.mp4"
-        videoInterface = VideoInterface(pathToVideo)
+        videoInterface = VideoInterface(self.testVideoPath)
         frame = videoInterface.get_frame()
         self.assertIsInstance(frame, np.ndarray)
 
     def test_can_create_representation_from_frame(self):
-        pass
-
-        # pathToFrame = './test1.jpg'
-        # single_file = SingleFile()
-        # testImage = single_file.getImageFromFile(pathToFrame)
-        # rep = single_file.getRep(testImage)
-        # self.assertIsInstance(rep, numpy.ndarray, "Representation created was wrong type; should be array")
+        videoInterface = VideoInterface()
+        frame = videoInterface.get_image_from_file(self.testImagePath)
+        faceRepresentation = self.faceRecognition.get_rep(frame, self.defaultImageDims)
+        self.assertIsInstance(faceRepresentation, np.ndarray, "Representation created was wrong type; should be array")
 
 
 if __name__ == '__main__':
