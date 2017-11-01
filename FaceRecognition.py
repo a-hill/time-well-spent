5c5e1884a0e11bceeacecf50d06ef6686a573908
Modern Times <ai2015@imperial.ac.uk>
2017-11-01 16:03:18 +0000

    Added functionality to detect multiple faces in a frame. Is untested and requires test files

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -4,6 +4,7 @@ import numpy as np
 from VideoInterface import VideoInterface
 import random
 
+
 class FaceRecognition():
     DEFAULT_IMAGE_DIMENSION = 96
 
@@ -14,11 +15,21 @@ class FaceRecognition():
 
     def get_rep(self, image):
         alignedFace = self.align_face(image)
-        if alignedFace is None: # Alignment failed
+        if alignedFace is None:  # Alignment failed
             return None
         else:
             rep = self.net.forward(alignedFace)
             return rep
+
+    def get_reps(self, image):
+        faces = self.align_faces(image)
+        if len(faces) == 0:  # Alignment failed
+            return None
+        else:
+            reps = []
+            for face in faces:
+                reps.append(self.net.forward(face))
+            return reps
 
     # private function
     def align_face(self, image):
@@ -32,14 +43,36 @@ class FaceRecognition():
             return None
 
         # Crops and rotates according to bb
-        alignedFace = self.aligner.align(self.DEFAULT_IMAGE_DIMENSION, rgbImg, bb, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+        alignedFace = self.aligner.align(self.DEFAULT_IMAGE_DIMENSION, rgbImg, bb,
+                                         landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
 
         if alignedFace is None:  # Alignment failed
             return None
         return alignedFace
 
+    #private function
+    def align_faces(self, image):
+        # Converts image to format expected by aligner
+        rgbImg = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        faces = self.aligner.getAllFaceBoundingBoxes(rgbImg)
+
+        # No face found in frame
+        if len(faces) == 0:
+            return None
+
+        alignedFaces = []
+        # Crops and rotates each bounding box in the frame
+        for face in faces:
+            aligned = self.aligner.align(self.DEFAULT_IMAGE_DIMENSION, rgbImg, face,
+                                         landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+            if aligned is not None:
+                alignedFaces.append(aligned)
+
+        return alignedFaces
+
     def is_same_person(self, rep1, rep2):
-        if rep2 is not None and rep1 is not None: # Check they both exist
+        if rep2 is not None and rep1 is not None:  # Check they both exist
             d = rep1 - rep2
             distance = np.dot(d, d)
-            return distance < 0.99 # This number comes from openface docs
+            return distance < 0.99  # This number comes from openface docs
