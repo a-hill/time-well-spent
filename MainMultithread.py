218fb660e9144aed0d607ca439eb2caa2eb551aa
riajha <ria.jha15@imperial.ac.uk>
2017-11-02 15:27:41 +0000

    Added some comments to main multithread file

--- MainMultithread.py
+++ MainMultithread.py
@@ -6,8 +6,8 @@ import time
 
 class Main():
     def __init__(self):
-        self.entry = Queue()
-        self.exit = Queue()
+        self.entryQueue = Queue()
+        self.exitQueue = Queue()
         self.faceDetectors = []
         self.facesInRoom = []
 
@@ -18,21 +18,25 @@ class Main():
 
         for camera in cameras:
             if camera[1] == "entry":
-                self.faceDetectors.append(FaceDetector(camera[0], self.entry))
+                #Set up an entrance camera
+                self.faceDetectors.append(FaceDetector(camera[0], self.entryQueue))
             else:
-                self.faceDetectors.append(FaceDetector(camera[0], self.exit))
+                #Set up an exit camera
+                self.faceDetectors.append(FaceDetector(camera[0], self.exitQueue))
 
     def run(self):
         while True:
             # check for people entering
-            if not self.entry.empty():
-                elem = self.entry.get()
+            if not self.entryQueue.empty():
+                #Get an element from the output queue and check if this person already exists in the list
+                elem = self.entryQueue.get()
                 if not any(FaceRecognition.is_same_person(x[0], elem[0]) for x in self.facesInRoom):
+                    #Adding any new people ie. those that aren't found in the list
                     self.facesInRoom.append(elem)
 
             # check for people exiting
-            if not self.exit.empty():
-                elem = self.entry.get()
+            if not self.exitQueue.empty():
+                elem = self.entryQueue.get()
                 newFacesInRoom = []
                 for face in self.facesInRoom:
                     if FaceRecognition.is_same_person(face[0], elem[0]):
@@ -41,5 +45,3 @@ class Main():
                         newFacesInRoom.append(face)
 
                 self.facesInRoom = newFacesInRoom
-
-
