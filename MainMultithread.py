830571c379888022c06495185cb7de1d16e207a7
riajha <ria.jha15@imperial.ac.uk>
2017-11-02 15:00:36 +0000

    FaceDetector is now appending the vector rep to the result array which it is now doing. MainMultithread now calls static function is_same_person from FaceRecognition

--- MainMultithread.py
+++ MainMultithread.py
@@ -1,50 +1,45 @@
 from multiprocessing import Queue
 from FaceDetector import FaceDetector
 from FaceRecognition import FaceRecognition
+import time
+
 
 class Main():
-	def __init__(self):
-		self.entry = Queue()
-		self.exit = Queue()
-		self.faceDetectors = []
-		self.facesInRoom = []
+    def __init__(self):
+        self.entry = Queue()
+        self.exit = Queue()
+        self.faceDetectors = []
+        self.facesInRoom = []
 
-		cameras = [
-			[0, "entry"],
-			[1, "exit"]
-		]
+        cameras = [
+            [0, "entry"],
+            [1, "exit"]
+        ]
 
-		for camera in cameras:
-			if camera[1] == "entry":
-				self.faceDetectors.append(FaceDetector(camera[0], self.entry))
-			else:
-				self.faceDetectors.append(FaceDetector(camera[0], self.exit))
+        for camera in cameras:
+            if camera[1] == "entry":
+                self.faceDetectors.append(FaceDetector(camera[0], self.entry))
+            else:
+                self.faceDetectors.append(FaceDetector(camera[0], self.exit))
 
-	def run(self):
-		while True:
-			#check for people entering
-			if not self.entry.empty():
-				elem = self.entry.get()
-				if not any(self.is_same_person(x[0], elem[0]) for x in self.facesInRoom):
-					self.facesInRoom.append(elem)
+    def run(self):
+        while True:
+            # check for people entering
+            if not self.entry.empty():
+                elem = self.entry.get()
+                if not any(FaceRecognition.is_same_person(x[0], elem[0]) for x in self.facesInRoom):
+                    self.facesInRoom.append(elem)
 
-			#check for people exiting
-			if not self.exit.empty():
-				elem = self.entry.get()
-				newFacesInRoom = []
-				for face in self.facesInRoom:
-					if self.is_same_person(face[0], elem[0]):
-						print("This person was in the room for: ", time.time() - face[1], " seconds")
-					else:
-						newFacesInRoom.append(face)
+            # check for people exiting
+            if not self.exit.empty():
+                elem = self.entry.get()
+                newFacesInRoom = []
+                for face in self.facesInRoom:
+                    if FaceRecognition.is_same_person(face[0], elem[0]):
+                        print("This person was in the room for: ", time.time() - face[1], " seconds")
+                    else:
+                        newFacesInRoom.append(face)
 
-				self.facesInRoom = newFacesInRoom		
+                self.facesInRoom = newFacesInRoom
 
 
-	def is_same_person(self, rep1, rep2):
-        if len(rep1) != len(rep2):
-            return False
-
-        d = rep1 - rep2
-        distance = np.dot(d, d)
-        return distance < 0.99 # This number comes from openface docs		
