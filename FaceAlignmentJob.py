4d032f3d50ffc1ded5b3e9a591e0cc0f4322d14d
Your Name <you@example.com>
2017-11-27 15:31:49 +0000

    removed excessive print statements and an old redundant can_discard_face function

--- FaceAlignmentJob.py
+++ FaceAlignmentJob.py
@@ -1,7 +1,5 @@
 import StringIO
-from multiprocessing import Process
 import requests
-import openface
 import cv2
 import time
 from PIL import Image
@@ -11,7 +9,6 @@ class FaceAlignmentJob:
     BB_SIZE_THRESHOLD = 3500
 
     DEFAULT_IMAGE_DIMENSION = 96
-    OVERLAP_THRESHOLD = 0.8
 
     def __init__(self, frame, t, door, url, aligner):
         self.frame   = frame
@@ -19,17 +16,6 @@ class FaceAlignmentJob:
         self.door    = door
         self.aligner = aligner
         self.url     = url
-
-    def is_similar(self, a, b):
-        overlapArea = float(a.intersect(b).area())
-        factor = overlapArea / float(a.area()) 
-        print 'overlap factor: ' + str(factor)
-        return factor >= self.OVERLAP_THRESHOLD
-
-    def can_discard_face(self, face):
-        return False
-        #print type(face)
-        #return any(self.is_similar(face, oldFace) for oldFace in facesLastFrame)
 
     def run(self):
         start = time.clock()
@@ -42,26 +28,20 @@ class FaceAlignmentJob:
         faces = self.align_faces()
         print 'after aligning faces, faces length: ' + str(len(faces))
         for face in faces:
-            if self.can_discard_face(face):
-                print 'discarded a face'
-            else:
-                im = Image.fromarray(face)
-                buf = StringIO.StringIO()
-                im.save(buf, "JPEG", quality=10)
-                jpegface = buf.getvalue()
-                files = { 'upload_file' : jpegface }
-                print 'Request: about to send'
-                requests.post(self.url, files=files, data=form)
+            im = Image.fromarray(face)
+            buf = StringIO.StringIO()
+            im.save(buf, "JPEG", quality=10)
+            jpegface = buf.getvalue()
+            files = { 'upload_file' : jpegface }
+            requests.post(self.url, files=files, data=form)
         
         print 'This face took: ' + str(time.clock() - start) + ' seconds to align.'
 
     def align_faces(self):
         # Converts image to format expected by aligner
         rgbImg = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
-        #rgbImg = self.frame
 
         faces = self.aligner.getAllFaceBoundingBoxes(rgbImg)
-        print 'got ' + str(len(faces)) + ' bounded boxes'
         alignedFaces = []
         # Crops and rotates each bounding box in the frame
         for face in faces:
