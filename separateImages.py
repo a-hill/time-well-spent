8e08afaf29969b02494fe7c10ff74384bb3af567
riajha <ria.jha15@imperial.ac.uk>
2018-01-06 23:39:01 +0000

    Added in a script that should separate matching images from final faces into another directory for each person. Will need to change folder names and file paths

---
+++ separateImages.py
@@ -0,0 +1,82 @@
+import os
+import cv2
+import openface
+import numpy as np
+
+def separate(door_dir):
+    #Go through each image in door_dir
+    i = 0
+    for fname in os.listdir(door_dir):
+        if fname != ".DS_Store":
+            print fname
+            print "onto new person"
+            #Create new directory for each image
+            newDirName = "Person" + str(i)
+            os.mkdir("./separated_door1/" + newDirName)
+            #Go through each image and search for match in all 4 directories
+            findMatch(dirA, newDirName, fname)
+            findMatch(dirB, newDirName, fname)
+            findMatch(dirC, newDirName, fname)
+            findMatch(dirD, newDirName, fname)
+            i+=1
+
+
+def getRep(img):
+    #img should already be in bgr
+    if img is not None:
+        rep = net.forward(img)
+        return rep
+    else:
+        return None
+
+
+def faceMatch(repEn, repEx):
+    if repEn is None or repEx is None:
+        return False
+
+    if len(repEn) != len(repEx):  # Check they're same length
+        return False
+
+    d = repEn - repEx
+    dot = np.dot(d, d)
+
+    return dot < 1.8
+
+
+def findMatch(searchDir, personDir, origFName):
+    #Check if the given image matches any in searchDir
+    for fname in os.listdir(searchDir):
+        #only check exit images
+        if "exit" in fname:
+            #Get the time and compare against entry
+            splitFName = fname.split("-")
+            exitTime = splitFName[0]
+            splitOrigName = origFName.split("-")
+            entryTime = splitOrigName[0]
+            if exitTime > entryTime:
+                rgbEntry = cv2.imread("/Users/riajha/modern-times-server/door1/" + origFName, 1)
+                bgrExit =  cv2.imread("/Users/riajha/modern-times-server/" + searchDir + "/"+fname, 1)
+                imEntry = cv2.cvtColor(rgbEntry, cv2.COLOR_RGB2BGR)
+                repEn = getRep(imEntry)
+                repEx = getRep(bgrExit)
+                if faceMatch(repEn, repEx):
+                    #save origFname in personDir
+                    print "match found, saving"
+                    cv2.imwrite("/Users/riajha/modern-times-server/separated_door1/" + personDir + "/"+ origFName, rgbEntry)
+
+
+
+
+networkModelPath = "/Users/riajha/openface/models/openface/nn4.small2.v1.t7"
+facePredictorPath = "/Users/riajha/openface/models/dlib/shape_predictor_68_face_landmarks.dat"
+net = openface.TorchNeuralNet(networkModelPath, 96)
+
+dirName = "/Users/riajha/modern-times-server/door1"
+dirA = "/Users/riajha/modern-times-server/facesA"
+dirB = "/Users/riajha/modern-times-server/facesB"
+dirC = "/Users/riajha/modern-times-server/facesC"
+dirD = "/Users/riajha/modern-times-server/facesD"
+
+
+
+separate(dirName)
