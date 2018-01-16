ae89dda47eff7cdfb83aae73bdecdd66db8ccc6e
riajha <ria.jha15@imperial.ac.uk>
2018-01-16 18:07:41 +0000

    Test added to check if multiple faces can be detected in a single frame

--- TestMultipleEntry.py
+++ TestMultipleEntry.py
@@ -1,8 +1,7 @@
 import unittest
-from FaceRecognition import FaceRecognition
+from FaceAlignmentJob import FaceAlignmentJob
 from VideoInterface import VideoInterface
-import cv2
-
+import openface
 
 class TestFacialRecognition(unittest.TestCase):
 
@@ -10,55 +9,22 @@ class TestFacialRecognition(unittest.TestCase):
     def setUpClass(self):
         self.pathToDLibFacePredictor = './../openface/models/dlib/shape_predictor_68_face_landmarks.dat'
         self.pathToTorchNeuralNet = './../openface/models/openface/nn4.small2.v1.t7'
-        self.faceRecognition = FaceRecognition(self.pathToDLibFacePredictor, self.pathToTorchNeuralNet)
-        self.testSingleFilePath = "./test_data/tate-1/angle1-threefacessinglefile.mov"
-        self.testStoreOneFace = "./test_data/tate-1/angle1-oneface.mov"
+        self.testMultipleFaces = "../modern-times-test-resources/not-tate/test_mult.mov"
 
+    def test_can_detect_multiple_faces_in_frame(self):
+        videoInterface = VideoInterface(self.testMultipleFaces)
+        aligner = openface.AlignDlib(self.pathToDLibFacePredictor)
+        # Find a non empty frame,
+        # Initially, there are only two faces in the frame
+        frame = None
+        while frame is None:
+            frame, t = videoInterface.get_frame_and_time()
 
-    # def test_can_store_face_on_entrace(self):
-    #     videoInterface = VideoInterface(self.testStoreOneFace)
-    #     face_reps = []
-    #     frame = "Nonsense"
-    #     duplicate = False
-    #     while frame is not None:
-    #         frame = videoInterface.get_frame()
-    #         if frame is not None:
-    #             currentRep = self.faceRecognition.get_rep(frame)
-    #             if currentRep is not None:
-    #                 for f in face_reps:
-    #                     if self.faceRecognition.is_same_person(currentRep, f):
-    #                         duplicate = True
-    #                         break
-    #                     else:
-    #                         duplicate = False
-    #                 if not duplicate:
-    #                     face_reps.append(currentRep)
-    #     print (len(face_reps))
-    #     self.assertTrue(len(face_reps) == 1)
+        faceAlignmentJob = FaceAlignmentJob(frame, t, 0, "", aligner)
+        facesInRoom = faceAlignmentJob.align_faces()
 
-
-    def test_can_store_multiple_faces_on_entrance(self):
-        video_interface = VideoInterface(self.testSingleFilePath)
-        reps = []
-        f = "Nonsense"
-        while f is not None:
-            f = video_interface.get_frame()
-            if f is not None:
-                all_faces = self.faceRecognition.get_reps(f)
-                if len(all_faces) > 0:
-                    for face in all_faces:
-                        already_exists_in_list = self.find_face_in_list(reps, face)
-                        if not already_exists_in_list:
-                            reps.append(face)
-        print len(reps)
-        self.assertTrue(len(reps) == 3)
-
-
-    def find_face_in_list(self, reps, face):
-            for r in reps:
-                if self.faceRecognition.is_same_person(face, r):
-                    return True
-            return False
+        print len(facesInRoom)
+        self.assertTrue(len(facesInRoom) == 2)
 
 
 if __name__ == '__main__':
